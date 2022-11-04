feda293ea1d85f9250719c58ffefdc795a569b0a
kastahov <kastahov1021@gmail.com>
2022-11-04 14:07:19 +0200

    Add ext-pcntl exception

---
+++ worker.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+use Spiral\RoadRunner;
+use Nyholm\Psr7;
+
+include "vendor/autoload.php";
+
+$worker = RoadRunner\Worker::create();
+$psrFactory = new Psr7\Factory\Psr17Factory();
+
+$worker = new RoadRunner\Http\PSR7Worker($worker, $psrFactory, $psrFactory, $psrFactory);
+
+while ($req = $worker->waitRequest()) {
+    try {
+        $rsp = new Psr7\Response();
+        $rsp->getBody()->write('Hello world!');
+
+        $worker->respond($rsp);
+    } catch (\Throwable $e) {
+        $worker->getWorker()->error((string)$e);
+    }
+}
