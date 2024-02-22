6791dc9834fcc890916e7d4beadf4bbc2ead04f3
Maxim Smakouz <m.smakouz@gmail.com>
2024-02-23 00:32:45 +0200

    Improve comment

--- src/WorkerPool.php
+++ src/WorkerPool.php
@@ -42,7 +42,7 @@ final class WorkerPool
     }
 
     /**
-     * Get the worker count for a pool.
+     * Get the number of workers for the pool.
      *
      * @param non-empty-string $plugin
      */
@@ -52,7 +52,7 @@ final class WorkerPool
     }
 
     /**
-     * Get the info about running workers for a pool.
+     * Get the info about running workers for the pool.
      *
      * @param non-empty-string $plugin
      */
